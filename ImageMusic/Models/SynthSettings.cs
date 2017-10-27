2ff38131ab872788e9d41a7a4033f8b8fa45369e
Morooney <1309022@MBP-1309022-01.corp.gianteagle.com>
2017-10-27 11:41:01 -0400

    adds basic synth settings singleton

---
+++ ImageMusic/Models/SynthSettings.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace ImageMusic
+{
+    public class SynthSettings
+    {
+        #region Properties
+
+        public Dictionary<ColorComponent, TargetModifier> CurrentSettings { get; set; }
+
+        #endregion
+
+        #region Singleton implementation
+
+        public SynthSettings Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new SynthSettings();
+                }
+
+                return _Instance;
+            }
+        }
+
+        #endregion
+
+        #region Construction and defaults
+
+        SynthSettings()
+        {
+            SetupDefaults();
+        }
+
+        SynthSettings _Instance;
+
+        void SetupDefaults()
+        {
+            CurrentSettings = new Dictionary<ColorComponent, TargetModifier>
+            {
+                { ColorComponent.Red, TargetModifier.CarrierFrequency },
+                { ColorComponent.Green, TargetModifier.Octave },
+                { ColorComponent.Blue, TargetModifier.ModifierFrequency },
+                { ColorComponent.Brightness, TargetModifier.NoteLength }
+            };
+        }
+
+        #endregion
+    }
+}
